13b48d8265b3a9ca591fb970a00e83213a45defd
João Guilherme <joaovonlinde@gmail.com>
2025-09-29 23:59:44 -0300

    Ajusta tudo novamente para o nginx

--- run.py
+++ run.py
@@ -4,12 +4,5 @@ from flask_migrate import Migrate
 app = create_app()
 migrate = Migrate(app, db)
 
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        ssl_context=(
-            "/certs/fullchain.pem",
-            "/certs/privkey.pem"
-        )
-    )
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
